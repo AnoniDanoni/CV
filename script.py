2f891dd793bdded8c384f218afd57a9b06d6cdeb
Anderson Calisto dos Santos <anderson.contato.dev.com>
2026-08-25 14:16:17 -0300

    feat: observando formas geometricas

--- script.py
+++ script.py
@@ -12,13 +12,32 @@ contours, hierarchy = cv2.findContours(
     cv2.CHAIN_APPROX_SIMPLE
 )
 
-resultado = img.cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+resultado = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
 for cnt in contours:
-    cv2.drawContours(resultado, [cnt], -1, (255, 0, 0), 0)
-    x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(img, (x,y),(x+w, y+h), (255,0,0), 2)
+    area = cv2.contourArea(cnt)
 
-cv2.imshow("contornos", img)
+    if area > 500:
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+        pontas = len(approx)
+
+        x,y,w,h = cv2.boundingRect(approx)
+        cv2.drawContours(resultado, [approx], -1, (0,255,0), 2)
+        cv2.rectangle(resultado, (x,y),(x+w + 2, y+h + 2), (255,0,0), 2)
+        cv2.putText(resultado, str(pontas), (x, y - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2)
+
+        if pontas == 2:
+            pontas = "LINHA"
+        elif pontas == 3:
+            pontas = "TRIANGULO"
+        elif pontas == 4:
+            pontas = "QUADRADO"
+        elif pontas > 4:
+            pontas = "CIRCULO"
+        print(f"area: {area:.0f} | perimetro: {peri:.0f} | pontas: {pontas}")
+
+cv2.imshow("contornos", resultado)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
